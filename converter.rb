8e50a583b00498be7cd806987c481cd1c9225207
Melissa Kaulfuss <melissacor3lfuss@gmail.com>
2016-05-28 11:45:21 +1000

    Refactor to reduce items in Hash –– write function to calculate roman numerals re-using "I" eg. I, II, III.

--- converter.rb
+++ converter.rb
@@ -1,21 +1,56 @@
-def roman(english_numerals)
-	return "I" if english_numerals == 1
-		"II"
+ROMAN_NUMERALS = { 1 => 'I',4=>'IV',5=> 'V',6=>'VI',7=> 'VII',8=> 'VIII',9=> 'IX',10=> 'X'}
+
+def roman_numerals(english_numeral)
+	#sh_numeral <= 3
+	if	[1,2,3].include? english_numeral
+		return 'I' * english_numeral
+	end
+
+
+	ROMAN_NUMERALS[english_numeral]
 end
 
 require "minitest/spec"
 require "minitest/autorun"
 require "minitest/pride"
 
-describe "roman" do
-  it "converts the number 1 to the string I" do
-    roman(1).must_equal "I"
- end
+describe "roman_numerals" do
+  it "converts the number 1 to the roman numeral I" do
+    roman_numerals(1).must_equal "I"
+  end
 
-describe "roman" do
-  it "converts the number 2 to the string II" do
-    roman(2).must_equal "II"
-end
+  it "converts the number 2 to the roman numeral II" do
+    roman_numerals(2).must_equal "II"
+  end
 
-end
+  it "converts the number 3 to the roman numeral III" do
+    roman_numerals(3).must_equal "III"
+  end
+
+  it "converts the number 4 to the roman numeral IV" do
+    roman_numerals(4).must_equal "IV"
+  end
+
+  it "converts the number 5 to the roman numeral V" do
+    roman_numerals(5).must_equal "V"
+  end
+
+  it "converts the number 6 to the roman numeral VI" do
+    roman_numerals(6).must_equal "VI"
+  end
+
+  it "converts the number 7 to the roman numeral VII" do
+    roman_numerals(7).must_equal "VII"
+  end
+	it "converts the number 8 to the roman numeral VIII" do
+    roman_numerals(8).must_equal "VIII"
+  end
+
+  it "converts the number 9 to the roman numeral IX" do
+    roman_numerals(9).must_equal "IX"
+  end
+
+  it "converts the number 10 to the roman numeral X" do
+    roman_numerals(10).must_equal "X"
+  end
 end
